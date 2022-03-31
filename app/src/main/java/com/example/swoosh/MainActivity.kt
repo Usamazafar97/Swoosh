d8cfadd03c78d8bcd55c989335f98e886f7130f5
usamazafar <osamazafar97@gmail.com>
2022-03-31 18:18:34 +0500

    lesson 1: Display screen completed lesson 2: League screen completed lesson 3: Intents and Context completed

--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        getStartedButton.setOnClickListener {
+
+            // intent for transfer the handle from current activity to League Activity
+            val leagueActivityIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueActivityIntent)
+        }
 
     }
 }
