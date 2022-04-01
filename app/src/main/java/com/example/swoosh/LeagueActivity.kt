ba85465b9d326e0868e2672afe0517f60fad5674
usamazafar <osamazafar97@gmail.com>
2022-04-01 14:42:14 +0500

    lesson 1: Display screen completed lesson 2: League screen completed lesson 3: Intents and Context completed - Button Background is not changing (toggling background) lesson 6: Skill Activity completed

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,5 +1,6 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -25,6 +26,13 @@ class LeagueActivity : AppCompatActivity() {
 //            womenBtn.isEnabled = !mensBtn.isEnabled
 
             Toast.makeText(this, "Women Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        nextBtn.setOnClickListener {
+
+            // intent for transfer the handle from current activity to League Activity
+            val skillActivityIntent = Intent(this, SkillActivity::class.java)
+            startActivity(skillActivityIntent)
         }
     }
 }
