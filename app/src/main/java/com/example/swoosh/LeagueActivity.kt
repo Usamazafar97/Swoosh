613d8773569f92fb05ff9f81f05cf02f3d21b075
usamazafar <osamazafar97@gmail.com>
2022-04-01 16:39:10 +0500

    lesson 1: Display screen completed lesson 2: League screen completed lesson 3: Intents and Context completed - Button Background changes Now lesson 6: Skill Activity completed - Data sent to Skill Activity

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
+
+    // passing the selected league to the next activity
+    var leagueSelected = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
@@ -16,25 +19,73 @@ class LeagueActivity : AppCompatActivity() {
 
             // changing background
             changeBackground(it)
+
+            // remove other button's background to normal
+            removeOtherButtonBackground(it)
+
+            // set the selected league to men
+            leagueSelected = "men"
         }
 
         womenBtn.setOnClickListener {
 
             // changing background
             changeBackground(it)
+
+            // remove other button's background to normal
+            removeOtherButtonBackground(it)
+
+            // set the selected league to women
+            leagueSelected = "women"
         }
 
         coedBtn.setOnClickListener {
 
             // changing background
             changeBackground(it)
+
+            // remove other button's background to normal
+            removeOtherButtonBackground(it)
+
+            // set the selected league to co-ed
+            leagueSelected = "co-ed"
         }
 
         nextBtn.setOnClickListener {
 
-            // intent for transfer the handle from current activity to League Activity
-            val skillActivityIntent = Intent(this, SkillActivity::class.java)
-            startActivity(skillActivityIntent)
+            callSkillActivity(leagueSelected)
+        }
+    }
+
+    private fun callSkillActivity(leagueSelected: String) {
+
+        // intent for transfer the handle from current activity to League Activity
+        val skillActivityIntent = Intent(this, SkillActivity::class.java)
+
+        // adding the data to intent
+        skillActivityIntent.putExtra(LEAGUE_SELECTED, leagueSelected)
+
+        // calling the skill activity
+        startActivity(skillActivityIntent)
+    }
+
+    private fun removeOtherButtonBackground(view: View) {
+
+        // checking the view (button), and normalize the remaining buttons to normal background
+        when(view){
+            mensBtn -> {
+                womenBtn.setBackgroundResource(R.drawable.swoosh_button)
+                coedBtn.setBackgroundResource(R.drawable.swoosh_button)
+            }
+            womenBtn -> {
+                mensBtn.setBackgroundResource(R.drawable.swoosh_button)
+                coedBtn.setBackgroundResource(R.drawable.swoosh_button)
+            }
+            coedBtn -> {
+                womenBtn.setBackgroundResource(R.drawable.swoosh_button)
+                mensBtn.setBackgroundResource(R.drawable.swoosh_button)
+            }
+
         }
     }
 
--- app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -2,10 +2,17 @@ package com.example.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class SkillActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        // getting the data from the previous activity
+        val selectedLeague = intent.getStringExtra(LEAGUE_SELECTED)
+
+        Toast.makeText(this,selectedLeague,Toast.LENGTH_SHORT).show()
+
     }
 }
