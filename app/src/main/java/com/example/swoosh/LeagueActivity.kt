037de976798bc52e5ca4b9913974e7db5078568e
usamazafar <osamazafar97@gmail.com>
2022-04-01 12:24:02 +0500

    lesson 1: Display screen completed lesson 2: League screen completed lesson 3: Intents and Context completed - Button Background is not changing (toggling background)

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -2,10 +2,29 @@ package com.example.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        mensBtn.setOnClickListener {
+
+            // toggling
+//            mensBtn.isEnabled = !mensBtn.isEnabled
+
+//            mensBtn.isEnabled = true
+
+            Toast.makeText(this, "Men Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        womenBtn.setOnClickListener {
+
+//            womenBtn.isEnabled = !mensBtn.isEnabled
+
+            Toast.makeText(this, "Women Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
